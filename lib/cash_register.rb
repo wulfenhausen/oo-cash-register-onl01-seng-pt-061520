16f9fd60e1fae597a4bc659aef92560dd9fcbda7
William Han Findiesen <william.findiesen@gmail.com>
2020-07-26 21:28:07 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,11 @@
-
+def Cash_Register
+  attr_accessor :price, :quantity, :discount_percentage
+  
+  def initialize
+    @price = price
+    @quantity = quantity
+    @discount_percentage = discount_percentage
+    
+  end
+  
+end
